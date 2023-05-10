dbe0bf81686543103aaec8a7730612c515f8a98a
uzuran <archloverlinux@gmail.com>
2023-05-10 19:22:02 +0200

    -Create basic structure for MVVM architecture.

---
+++ Models/MyModel.cs
@@ -0,0 +1,13 @@
+﻿using SQLite;
+
+
+namespace ContactList.Models
+{
+    public class MyModel
+    {
+        [PrimaryKey, AutoIncrement]
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int password { get; set; }
+    }
+}

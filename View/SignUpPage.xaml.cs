7e7993736194e6b3b2aeb598b457d47528a01129
uzuran <archloverlinux@gmail.com>
2023-06-13 12:15:43 +0200

    - Add SignupViewModel.cs for mvvm architecture.

--- View/SignUpPage.xaml.cs
+++ View/SignUpPage.xaml.cs
@@ -1,5 +1,3 @@
-using ContactList.Models;
-using Microsoft.EntityFrameworkCore;
 
 namespace ContactList.Pages;
 
@@ -8,42 +6,7 @@ public partial class SignUpPage : ContentPage
     public SignUpPage()
 	{
 		InitializeComponent();
-    }
-    private void OnRegisterClicked(object sender, EventArgs e)
-    {
-        string userName = UsernameEntry.Text;
-        string userPassword = PasswordEntry.Text;
 
-        UserModel user = new UserModel
-        {
-            Username = userName,
-            Password = userPassword
-        };
-
-        try
-        {
-            using (var db = new UserDataContext())
-            {
-                db.Users.Add(user);
-                db.SaveChanges();
-            }
-        }
-        
-        catch (DbUpdateException ex)
-        {
-            Exception innerException = ex.InnerException;
-
-            // Extract details from the inner exception
-            string errorMessage = innerException.Message;
-            string stackTrace = innerException.StackTrace;
-
-            DisplayAlert(title: "Error", message: errorMessage, cancel: "Exit");
-
-            // Handle or log the error details
-        }
-
-        // Display a success message or navigate to another page
-        // after successful registration
     }
 
 }
---
+++ ViewModel/SignUpViewModel.cs
@@ -0,0 +1,60 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+using ContactList.Models;
+using Microsoft.EntityFrameworkCore;
+using System.Windows.Input;
+
+namespace ContactList.ViewModels
+{
+    public class SignUpViewModel : ObservableObject
+    {
+        private string username;
+        public string Username
+        {
+            get { return username; }
+            set { SetProperty(ref username, value); }
+        }
+
+        private string password;
+        public string Password
+        {
+            get { return password; }
+            set { SetProperty(ref password, value); }
+        }
+
+        public ICommand RegisterCommand { get; }
+
+        public SignUpViewModel()
+        {
+            RegisterCommand = new RelayCommand(Register);
+        }
+
+        private void Register()
+        {
+            UserModel user = new UserModel
+            {
+                Username = Username,
+                Password = Password
+            };
+
+            try
+            {
+                using (var db = new UserDataContext())
+                {
+                    db.Users.Add(user);
+                    db.SaveChanges();
+                }
+            }
+            catch (DbUpdateException ex)
+            {
+                Exception innerException = ex.InnerException;
+
+                // Extract details from the inner exception
+                string errorMessage = innerException.Message;
+                string stackTrace = innerException.StackTrace;
+
+
+            }
+        }
+    }
+}
